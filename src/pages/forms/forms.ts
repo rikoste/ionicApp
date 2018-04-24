86fca5bc1a4393adc3d9430d9a77465bbcd449ad
Matthieu Bouchez <bouchez-m@ch-v.net>
2018-04-24 18:54:42 +0200

    Forms validation

--- src/pages/forms/forms.ts
+++ src/pages/forms/forms.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
 import { GlobalData } from '../../providers/globaldata/globaldata';
 import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-forms',
@@ -13,13 +15,30 @@ export class FormsPage {
   title: any;
 	data: any;
   formData: any;
+  formValid: any;
+  private formGroup : FormGroup = new FormGroup({});
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public globalData: GlobalData, public alertCtrl: AlertController) {
-    this.formData = [];
+  constructor(private formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public globalData: GlobalData, public alertCtrl: AlertController) {
+    this.formData  = [];
+    this.formValid = {};
   }
 
   subForm() {
     console.log(this.formData);
+
+    if(!this.formGroup.valid){
+      let alert = this.alertCtrl.create({
+          title: '',
+          subTitle: 'Certains champs ne sont pas valides.',
+          buttons: [{
+              text: 'Ok'
+          }]
+      });
+
+      alert.present();
+    }else{
+      console.log('OK');
+    }
   }
 
   ionViewDidEnter(){
@@ -39,17 +58,37 @@ export class FormsPage {
       alert.present();
     }else{
         this.title = this.globalData.getTitle();
-        this.data = this.globalData.getGlobalData();
+        this.data = this.globalData.getGlobalData(); 
 
         /*for(var i = 0; i < this.data.length; i++){
-          this.data[i].label = this.data[i].value;
+          if(this.data[i].section != "title"){
+            if(this.data[i].mandatory == "true"){
+              this.formValid[this.data[i].value].push(Validators.required);
+            }else{
+              this.formValid[this.data[i].value] = ['default value if needed'];
+            }
+            
+          }
         }*/
-        
+
+        this.data.forEach(element => {
+            var key = element.value;
+            this.formValid[key] = [''];
+
+            if (element.section != "title" && element.mandatory == "true") {
+                this.formValid[key].push(Validators.required);
+            }
+        });
+
+        this.formGroup = this.formBuilder.group(this.formValid);
     };
 
-
+    // [DEBUG]
   	console.log('Données chargées dans forms : ');
     console.log(this.data);
+
+    console.log('Form valid : ');
+    console.log(this.formValid);
 
   }
 
