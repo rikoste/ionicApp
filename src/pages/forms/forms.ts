b72cbe5fc5a0bf81ea727ab90fb6321b31d8ac64
Matthieu Bouchez <bouchez-m@ch-v.net>
2018-04-25 09:35:29 +0200

    Rollback form validation

--- src/pages/forms/forms.ts
+++ src/pages/forms/forms.ts
@@ -15,30 +15,15 @@ export class FormsPage {
   title: any;
 	data: any;
   formData: any;
-  formValid: any;
-  private formGroup : FormGroup = new FormGroup({});
 
   constructor(private formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public globalData: GlobalData, public alertCtrl: AlertController) {
     this.formData  = [];
-    this.formValid = {};
   }
 
   subForm() {
+
     console.log(this.formData);
 
-    if(!this.formGroup.valid){
-      let alert = this.alertCtrl.create({
-          title: '',
-          subTitle: 'Certains champs ne sont pas valides.',
-          buttons: [{
-              text: 'Ok'
-          }]
-      });
-
-      alert.present();
-    }else{
-      console.log('OK');
-    }
   }
 
   ionViewDidEnter(){
@@ -59,37 +44,11 @@ export class FormsPage {
     }else{
         this.title = this.globalData.getTitle();
         this.data = this.globalData.getGlobalData(); 
-
-        /*for(var i = 0; i < this.data.length; i++){
-          if(this.data[i].section != "title"){
-            if(this.data[i].mandatory == "true"){
-              this.formValid[this.data[i].value].push(Validators.required);
-            }else{
-              this.formValid[this.data[i].value] = ['default value if needed'];
-            }
-            
-          }
-        }*/
-
-        this.data.forEach(element => {
-            var key = element.value;
-            this.formValid[key] = [''];
-
-            if (element.section != "title" && element.mandatory == "true") {
-                this.formValid[key].push(Validators.required);
-            }
-        });
-
-        this.formGroup = this.formBuilder.group(this.formValid);
     };
 
     // [DEBUG]
   	console.log('Données chargées dans forms : ');
     console.log(this.data);
-
-    console.log('Form valid : ');
-    console.log(this.formValid);
-
   }
 
 }
